47ed5ef765db736a2f786f10d6f7043b34991525
Christopher Dedominici <18092467+ChristopherDedominici@users.noreply.github.com>
2023-11-17 01:59:41 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+if (process.argv.length < 3) {
+  throw new Error("No command provided");
+}
+
+if (process.argv[2] === "test") {
+  console.log("test command working");
+} else {
+  console.log("command not found");
+}
