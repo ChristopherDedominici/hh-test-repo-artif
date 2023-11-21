2756bf95e1ee8f6c181d1d2da1ca700a5b83b6d9
Christopher Dedominici <18092467+ChristopherDedominici@users.noreply.github.com>
2023-11-21 19:30:46 +0100

    export function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-if (process.argv.length < 3) {
-  throw new Error("No command provided");
+function test() {
+  if (process.argv.length < 3) {
+    throw new Error("No command provided");
+  }
+
+  if (process.argv[2] === "test") {
+    console.log("test command working");
+  } else {
+    console.log("command not found");
+  }
 }
 
-if (process.argv[2] === "test") {
-  console.log("test command working");
-} else {
-  console.log("command not found");
-}
+module.exports = test;
